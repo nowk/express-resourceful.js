09a42776560a2a3cc495732ec61d29cb73f16f78
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-03-23 00:23:30 -0400

    Clean up my test

--- test/index.js
+++ test/index.js
@@ -1,67 +1,34 @@
 /* jshint laxcomma: true, esnext: true, node: true */
 
-var co = require('co');
-var Q = require('q');
-var assert = require('chai').assert;
-var request = require('supertest');
-var express = require('express');
-var Resourceful = require('..');
+const co = require('co');
+const Q = require('q');
+const assert = require('chai').assert;
+const request = require('supertest');
+const express = require('express');
 
-var app = express();
-
-var nextMiddleware = function(req, res, next) {
-  next();
-};
-
-var authMiddleware = function(req, res, next) {
-  res.send({}, 401);
-};
-
-var actionfn = function(req, res, next) {
-  var params = {};
-  for(var key in req.params) {
-    params[key] = req.params[key];
-  }
-  res.send(params);
-};
-
-var controller = {
-  index: actionfn,
-  new: actionfn,
-  create: actionfn,
-  show: actionfn,
-  edit: actionfn,
-  update: actionfn,
-  patch: actionfn,
-  destroy: actionfn
-};
-
-var controllerWithMiddleware = {
-  index: [authMiddleware, actionfn],
-  new: [authMiddleware, actionfn],
-  create: [authMiddleware, actionfn],
-  show: [authMiddleware, actionfn]
-};
+const Resourceful = require('..');
+const app = express();
 
 
-describe('app.resources', function() {
-  before(function() {
+describe('#resources', function() {
+
+  'use strict';
+
+  let controller = {
+    index: fn,
+    new: fn,
+    create: fn,
+    show: fn,
+    edit: fn,
+    update: fn,
+    patch: fn,
+    destroy: fn
+  };
+
+  it('maps the default actions', function(done) {
     app.resources('/posts', controller);
-    app.resources('/comments', controllerWithMiddleware);
-    app.resources('/', controller); // / should always be defined last
-  });
 
-  it('maps default CRUD routes', function(done) {
     co(function *() {
-      yield assert_request('get',    '/',          200);
-      yield assert_request('get',    '/new',       200);
-      yield assert_request('get',    '/123',       200);
-      yield assert_request('post',   '/',          200);
-      yield assert_request('get',    '/123/edit',  200);
-      yield assert_request('put',    '/123',       200);
-      yield assert_request('patch',  '/123',       200);
-      yield assert_request('del',    '/123',       200);
-
       yield assert_request('get',    '/posts/',          200);
       yield assert_request('get',    '/posts/new',       200);
       yield assert_request('get',    '/posts/123',       200);
@@ -70,28 +37,38 @@ describe('app.resources', function() {
       yield assert_request('put',    '/posts/123',       200);
       yield assert_request('patch',  '/posts/123',       200);
       yield assert_request('del',    '/posts/123',       200);
-
       done();
     })();
   });
 
-  it('supports middlewares', function(done) {
+  it('middlewares', function(done) {
+    app.resources('/comments', {
+      index:   [auth, fn],
+      new:     fn,
+      create:  [auth, fn]
+    });
+
     co(function *() {
       yield assert_request('get',   '/comments',      401);
-      yield assert_request('get',   '/comments/new',  401);
+      yield assert_request('get',   '/comments/new',  200);
       yield assert_request('post',  '/comments',      401);
       done();
     })();
   });
 
   describe('nested resources', function() {
-    it('creates nested routes', function(done) {
+    let assignmentsResources;
+    let commentsResources;
+
+    before(function() {
       app.resources('/tasks', controller, function(tasks) {
-        tasks.resources('/assignments', controller, function(assignments) {
-          assignments.resources('/comments', controller);
+        assignmentsResources = tasks.resources('/assignments', controller, function(assignments) {
+          commentsResources = assignments.resources('/comments', controller);
         });
       });
+    });
 
+    it('creates nested routes', function(done) {
       co(function *() {
         yield assert_request('get',    '/tasks/123/assignments',           200);
         yield assert_request('get',    '/tasks/123/assignments/new',       200);
@@ -101,7 +78,12 @@ describe('app.resources', function() {
         yield assert_request('put',    '/tasks/123/assignments/456',       200);
         yield assert_request('patch',  '/tasks/123/assignments/456',       200);
         yield assert_request('del',    '/tasks/123/assignments/456',       200);
+        done();
+      })();
+    });
 
+    it('creates nested nested routes, and so on', function(done) {
+      co(function *() {
         yield assert_request('get',    '/tasks/123/assignments/456/comments',           200);
         yield assert_request('get',    '/tasks/123/assignments/456/comments/new',       200);
         yield assert_request('post',   '/tasks/123/assignments/456/comments',           200);
@@ -110,55 +92,60 @@ describe('app.resources', function() {
         yield assert_request('put',    '/tasks/123/assignments/456/comments/789',       200);
         yield assert_request('patch',  '/tasks/123/assignments/456/comments/789',       200);
         yield assert_request('del',    '/tasks/123/assignments/456/comments/789',       200);
-
         done();
       })();
     });
 
-    it('uses the singularized path as the prefix for the id param name', function(done) {
-      app.resources('/tasks', controller, function(tasks) {
-        tasks.resources('/assignments', controller, function(assignments) {
-          var comments = assignments.resources('/comments', controller);
-
-          assert.equal(assignments.path, '/tasks/:task_id/assignments');
-          assert.equal(comments.path, '/tasks/:task_id/assignments/:assignment_id/comments');
-          assert_request('get', '/tasks/123/assignments/456/comments/789',
-            {task_id: '123', assignment_id: '456', id: '789'}).then(function() { done(); });
-        });
-      });
+    it('uses the singularized namespace as the prefix for the id param name', function(done) {
+      assert.equal(assignmentsResources.path, '/tasks/:task_id/assignments');
+      assert.equal(commentsResources.path, '/tasks/:task_id/assignments/:assignment_id/comments');
+      assert_request('get', '/tasks/123/assignments/456/comments/789',
+        {task_id: '123', assignment_id: '456', id: '789'}).then(done());
     });
   });
 
-  it('creates a namespace if no actions are provided', function(done) {
+  it('namespaces if no actions are provided', function(done) {
     app.resources('/api', function(api) {
-      var posts = api.resources('/posts', controller);
+      let posts = api.resources('/posts', controller);
       assert.equal(posts.path, '/api/posts');
 
       co(function *() {
         yield assert_request('put', '/api/posts/123', {id: '123'});
-        // yield assert_request('get', '/api', 404); // TODO route load order is breaking test
+        yield assert_request('get', '/api', 404);
         done();
       })();
     });
   });
+
+  // This causes some trouble. Any route set after
+  // will get caught by this and return false postives
+  //
+  // it('maps default actions on root', function(done) {
+  //   app.resources('/', controller);
+
+  //   co(function *() {
+  //     yield assert_request('get',    '/',          200);
+  //     yield assert_request('get',    '/new',       200);
+  //     yield assert_request('get',    '/123',       200);
+  //     yield assert_request('post',   '/',          200);
+  //     yield assert_request('get',    '/123/edit',  200);
+  //     yield assert_request('put',    '/123',       200);
+  //     yield assert_request('patch',  '/123',       200);
+  //     yield assert_request('del',    '/123',       200);
+  //     done();
+  //   })();
+  // });
 });
 
 describe('app.resourceMatch', function() {
-  before(function() {
-    app.resourceMatch(Resourceful.verbs.GET, '/users', actionfn);
-    app.resourceMatch(Resourceful.verbs.DELETE, '/users/:id', [nextMiddleware, authMiddleware, actionfn]);
-  });
-
   it('maps a single route', function(done) {
-    assert_request('get', '/users', 200).then(function(res) {
-      done();
-    });
+    app.resourceMatch(Resourceful.verbs.GET, '/users', fn);
+    assert_request('get', '/users', 200).then(done());
   });
 
   it('supports middlewares', function(done) {
-    assert_request('del', '/users/123', 401).then(function(res) {
-      done();
-    });
+    app.resourceMatch(Resourceful.verbs.DELETE, '/users/:id', [nextMiddleware, auth, fn]);
+    assert_request('del', '/users/123', 401).then(done());
   });
 });
 
@@ -169,11 +156,50 @@ describe('app.resourceMatch', function() {
 
 function assert_request(method, path, status) {
   var defer = Q.defer();
+
   request(app)[method](path)
     .expect(status)
     .end(function(err, res) {
       if (err) throw err;
       defer.resolve(res);
     });
+
   return defer.promise;
 }
+
+
+/*
+ * just next
+ */
+
+function nextMiddleware(req, res, next) {
+  next();
+}
+
+
+/*
+ * mimic failed auth middleware
+ */
+
+function auth(req, res, next) {
+  res.send({}, 401);
+}
+
+
+/*
+ * middleware acts as action sends params back as body
+ */
+
+function fn(req, res, next) {
+
+  'use strict';
+
+  let params = {};
+
+  for(let key in req.params) {
+    params[key] = req.params[key];
+  }
+
+  res.send(params);
+}
+
